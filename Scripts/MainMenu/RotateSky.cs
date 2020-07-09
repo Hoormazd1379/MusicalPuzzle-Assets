63db77f52e99d95d8824a4a5318ac46e6d3f0e22
Hoormazd Pirayeshfar <42213001+Hoormazd1379@users.noreply.github.com>
2020-07-09 22:13:54 +0200

    init

---
+++ Scripts/MainMenu/RotateSky.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour
+{
+    public int speed = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0); //rotates 50 degrees per second around z axis
+    }
+}
